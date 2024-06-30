db312b8dafdf2cbba3a19ce2f4da2e7b34bcd646
Rehaan Ahmad <rehaansufyaan1@gmail.com>
2024-06-30 15:55:03 +0530

    game

---
+++ src/main/java/NUMBER_GAME.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.Random;
+import java.util.Scanner;
+
+public class NUMBER_GAME {
+    int randomnum;
+    int score =0;
+    void cpunum(){
+        Random  random=new Random();
+        this.randomnum=random.nextInt(100);
+    }
+   Boolean persion(int num){
+       if (num!=this.randomnum){
+           System.out.println("pleace try again ");
+           if (num<this.randomnum){
+               System.out.println("too lower");
+           }
+           else if (num>this.randomnum){
+               System.out.println("too higher");
+           }
+           score++;
+           return true;
+       }else {
+           System.out.println("won the game\nand the score is : "+this.score);
+           return false;
+       }
+
+    }
+    public static void main(String[] args) {
+NUMBER_GAME ob1=new NUMBER_GAME();
+        Scanner sc=new Scanner(System.in);
+ob1.cpunum();
+Boolean  t=true;
+while (t){
+    boolean ans=ob1.persion(sc.nextInt());
+    t=ans;
+}
+    }
+}
